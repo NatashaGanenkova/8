695135eea85138646066a5988e212eb18fd4c66e
Nata <ganenkova@mail.ru>
2023-02-28 21:44:24 +0300

    Задача64

---
+++ Zadacha64/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static System.Console;
+Write("Введите M: ");
+int m = int.Parse(ReadLine());
+Write("Введите N: ");
+int n = int.Parse(ReadLine());
+
+WriteLine(PrintNumbers(m, n));
+
+string PrintNumbers(int start, int end)
+{
+    if (end == start) return end.ToString();
+    return (end + " " + PrintNumbers(start, end - 1));
+}
