b9d225e2fabb7ec13caea4a639829a9636c5490c
Nata <ganenkova@mail.ru>
2023-03-01 08:49:53 +0300

    Задача 66

---
+++ Zadacha66/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static System.Console;
+Write("Введите M: ");
+int m = int.Parse(ReadLine());
+Write("Введите N: ");
+int n = int.Parse(ReadLine());
+
+WriteLine($"Cумма чисел = {SumNumbers(m, n)}");
+
+int SumNumbers(int start, int end)
+{
+    if (start == end) return start;
+    return (start + SumNumbers(start + 1, end));
+}
